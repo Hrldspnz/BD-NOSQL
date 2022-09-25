00235401437ebed85af84a156b649ab4df8ed009
FALCH2000 <faleiva.ch@gmail.com>
2022-09-25 01:17:36 -0600

    navbar admin update

--- GUI/GUI-Project1/src/app/app.routing.ts
+++ GUI/GUI-Project1/src/app/app.routing.ts
@@ -2,16 +2,17 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './components/login/login.component';
 import { RegisterComponent } from './components/register/register.component';
+import { AdminRoutingModule } from './components/admin/admin.routing.module';
 
 
 const routes: Routes = [
-    { path: '', redirectTo: 'app', pathMatch: 'full'},
+    { path: '', redirectTo: 'login', pathMatch: 'full'},
     { path: 'login', component: LoginComponent},
     { path: 'register', component: RegisterComponent},
-
-    { path: '**', redirectTo: 'app', pathMatch: 'full'}
+    { path: 'admin', loadChildren: () => import('./components/admin/admin.routing.module').then(x => x.AdminRoutingModule)},
+    { path: '**', redirectTo: 'login', pathMatch: 'full'}
   ];
-
+  
   @NgModule({
     imports: [RouterModule.forRoot(routes)],
     exports: [RouterModule]
