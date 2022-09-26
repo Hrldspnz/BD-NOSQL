d628eb0a962192eb5cb60032bbf573ba0dd494d9
FALCH2000 <faleiva.ch@gmail.com>
2022-09-26 09:47:48 -0600

    top and botton table

--- GUI/GUI-Project1/src/app/components/admin/tops/tops.component.ts
+++ GUI/GUI-Project1/src/app/components/admin/tops/tops.component.ts
@@ -1,12 +1,40 @@
 import { Component, OnInit } from '@angular/core';
 
+export interface top {
+  position: number;
+  nombre: string;
+  categoria: string;
+  cantidad: string;
+}
+
+
+
+
 @Component({
   selector: 'app-tops',
   templateUrl: './tops.component.html',
   styleUrls: ['./tops.component.css']
 })
 export class TopsComponent implements OnInit {
-
+  topClubs: top[] = [
+    {position: 1, nombre: '18', categoria: "Letras", cantidad:"3"},
+    {position: 2, nombre: '13', categoria: "sdf", cantidad:"3"},
+    {position: 3, nombre: '11', categoria: "rdfg", cantidad:"3"},
+    {position: 4, nombre: '11', categoria: "rdfg", cantidad:"3"},
+    {position: 5, nombre: '11', categoria: "rdfg", cantidad:"3"},
+    
+  ];
+  bottomClubs: top[] = [
+    {position: 1, nombre: 'u', categoria: "Letras", cantidad:"3"},
+    {position: 2, nombre: '13', categoria: "sdf", cantidad:"3"},
+    {position: 3, nombre: '11', categoria: "rdfg", cantidad:"3"},
+    {position: 4, nombre: '11', categoria: "rdfg", cantidad:"3"},
+    {position: 5, nombre: '11', categoria: "rdfg", cantidad:"3"},
+    
+  ];
+  displayedColumns: string[] = ['position', 'nombre', 'categoria', 'cantidad'];
+  dataSource = this.topClubs;
+  dataSource2= this.bottomClubs;
   constructor() { }
 
   ngOnInit(): void {
