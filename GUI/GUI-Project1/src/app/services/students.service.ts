96d0e3c6a42ae789f21de6a1db210ad93d26d6a0
Carmen Araya <valentinarayach64@gmail.com>
2022-09-26 20:23:45 -0600

    Update students.service.ts

--- GUI/GUI-Project1/src/app/services/students.service.ts
+++ GUI/GUI-Project1/src/app/services/students.service.ts
@@ -13,8 +13,21 @@ export class StudentsService {
     this.url = "http://127.0.0.1:5000/"
    }
 
-   addNewUser(dataUser: Object): Observable<any>{
+  addNewUser(dataUser: Object): Observable<any>{
     return this._http.post(this.url+'estudiantes', dataUser);
   }
+
+  addClub(dataClub: Object): Observable<any>{
+    return this._http.post(this.url+'cursos', dataClub);
+  }
+
+  getClub(dataClub: Object){
+    return this._http.get(this.url+'cursos', dataClub);
+  }
+
+  putInteresados(club: string, dataInter: Object){
+    return this._http.put(this.url+'cursos/' + club, dataInter);
+  }
+
 
 }
