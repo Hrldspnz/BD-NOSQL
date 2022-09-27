51b5e6b2a0f3a7a603ea2159b6ef49d685f98923
Carmen Araya <valentinarayach64@gmail.com>
2022-09-26 19:59:15 -0600

    Post registro

--- GUI/GUI-Project1/src/app/components/register/register.component.ts
+++ GUI/GUI-Project1/src/app/components/register/register.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
+import { StudentsService } from 'src/app/services/students.service';
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
@@ -11,9 +12,11 @@ export class RegisterComponent implements OnInit {
   formDataUser: FormGroup;
 
   constructor(private fb: FormBuilder,
-    private router: Router,) {
+    private router: Router,
+    private _userService: StudentsService) {
       this.formDataUser = this.fb.group ({
         name: ['', Validators.required],
+        user: ['', Validators.required],
         password: ['', Validators.required],
         passwordConfirm: ['', Validators.required],
         section: [''],
@@ -25,7 +28,24 @@ export class RegisterComponent implements OnInit {
   }
 
   addUser() {
-    console.log("Hola")
+    if (this.formDataUser.value.password != this.formDataUser.value.passwordConfirm){
+      alert ("Las constraseñas no coinciden")
+    } else {
+      const newUser: Object =
+      {
+        nombre: this.formDataUser.value.name,
+        usuario: this.formDataUser.value.user,
+        clave: this.formDataUser.value.password,
+        seccion: this.formDataUser.value.section,
+      };
+      this._userService.addNewUser(newUser).subscribe(data => {
+        console.log(data);
+        alert("Se han guardado los datos correctamente")
+      }, error => {
+        alert("Error al crear Cuenta de Usuario, dirección de Correo Electrónico ya registrada o contraseña menor a 8 caracteres")
+      }
+      );
+    }
   }
 
 }
--- GUI/GUI-Project1/src/app/services/students.service.ts
+++ GUI/GUI-Project1/src/app/services/students.service.ts
@@ -1,3 +1,5 @@
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 import { Injectable } from '@angular/core';
 
 @Injectable({
@@ -5,5 +7,14 @@ import { Injectable } from '@angular/core';
 })
 export class StudentsService {
 
-  constructor() { }
+  public url: string
+
+  constructor( public _http: HttpClient) {
+    this.url = "http://127.0.0.1:5000/"
+   }
+
+   addNewUser(dataUser: Object): Observable<any>{
+    return this._http.post(this.url+'estudiantes', dataUser);
+  }
+
 }
